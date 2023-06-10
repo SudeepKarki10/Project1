91a4ba3242b5a211383ab16df14d6da09c91ff52
Sudeep <sudeepkarki74@gmail.com>
2023-06-10 23:02:34 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMenu() {
+  const middleMenuGroup = document.querySelector('.middle-menu-group');
+  const lastMenuGroup = document.querySelector('.last-menu-group');
+  const menubarGroup = document.querySelector('.menu-logo')
+
+  if (middleMenuGroup.style.display === 'none') {
+    middleMenuGroup.style.display = 'flex';
+    lastMenuGroup.style.display = 'flex';
+    middleMenuGroup.style.flexDirection = 'column';
+    lastMenuGroup.style.flexDirection = 'column';
+    menubarGroup.style.display = 'none';
+  } else {
+    middleMenuGroup.style.display = 'none';
+    lastMenuGroup.style.display = 'none';
+  }
+}
+    function scrollToSection(sectionId) {
+      var section = document.getElementById(sectionId);
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
